6d0c61fb8567c6ab89821cdb33147a0e3870f455
shadowfox3735 <normanpan180@gmail.com>
2019-08-12 16:48:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,5 +15,9 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     params[:phrase] * params[:number].to_i
   end
+  
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    params[:word1] + " " +params[:word2] + " " + params[:word3] + " " + params[:word4] + " " + params[:word5]
+  end
 
 end

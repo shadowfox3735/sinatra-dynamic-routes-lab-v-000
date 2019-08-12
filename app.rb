1c1502c929f569ff8a1637021fb8b458a807dc9b
shadowfox3735 <normanpan180@gmail.com>
2019-08-12 16:36:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,5 +6,9 @@ class App < Sinatra::Base
     @reversed_name = params[:name].reverse!
     "#{@reversed_name}"
   end
+  
+  get '/square/:number' do
+    @squared = params[:number].to_i 
+  end
 
 end

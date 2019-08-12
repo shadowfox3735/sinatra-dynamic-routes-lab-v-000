1ecf8e992de22eee368c38a3c8f3fdc8655a7870
shadowfox3735 <normanpan180@gmail.com>
2019-08-12 16:33:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name'
+    @reversed_name = params[:name].reverse!
+    "#{@reversed_name}"
+  end
 
 end
